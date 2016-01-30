eab6258ce91a8eecd0bb8c6ef8dc2c4c24a85573
Jeyp <jp.evette1@gmail.com>
2016-01-30 02:02:04 +0000

    Adding random platform generation + Tin tin's movement script

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -12,7 +12,7 @@ public class GameMaster : MonoBehaviour {
 	}
     
 	public Transform playerPrefab;
-	public Transform spawnPoint;
+	static public Transform spawnPoint;
 	public int spawnDelay = 2;
 	public Transform SpawnParticalPrefab;
 	public Transform enemySpawn;
@@ -20,13 +20,14 @@ public class GameMaster : MonoBehaviour {
 	public IEnumerator RespawnPlayer (){
 		Debug.Log ("SpawnSound");
 		yield return new WaitForSeconds (spawnDelay);
-		Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
-		GameObject SpawnParticalClone = Instantiate (SpawnParticalPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+		Instantiate (playerPrefab, spawnPoint.position + new Vector3(0, 8, 0), spawnPoint.rotation);
+		GameObject SpawnParticalClone = Instantiate (SpawnParticalPrefab, spawnPoint.position + new Vector3(0, 8, 0), spawnPoint.rotation) as GameObject;
 		Destroy (SpawnParticalClone, 3f);
 	}
 
 
 	public static void KillPlayer(PlayerStats player){
+        spawnPoint = player.gameObject.GetComponent<PlayerMovement>().lastPlatformHit.transform;
 		Destroy (player.gameObject);
 		GM.StartCoroutine (GM.RespawnPlayer ());
 	}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 7.0f;
+    public float jumpY = 7.0f;
+    public GameObject lastPlatformHit;
+
+    private Animator anim;
+    private bool grounded = true;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetBool("Ground", grounded);
+        //jumpY -= gravity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.W) && grounded&& anim.GetBool("Ground"))
+        {
+            rb.velocity = new Vector3(0, jumpY, 0);
+            grounded = false;
+            anim.SetBool("Ground", false);
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        anim.SetFloat("vSpeed", rb.velocity.y);
+        anim.SetFloat("Speed", rb.velocity.x);
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rb.velocity = new Vector3(moveHorizontal * speed, rb.velocity.y, 0);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.tag == "Ground")
+        {
+            grounded = true;
+            lastPlatformHit = collider.gameObject;
+        }
+    }
+
+}
