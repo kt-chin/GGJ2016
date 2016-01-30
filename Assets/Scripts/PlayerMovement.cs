a4c01b2aa18a9d389467622d970f21bbd81831d6
Jeyp <jp.evette1@gmail.com>
2016-01-30 20:33:28 +0000

    Maybe Commit?

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float jumpY = 7.0f;
+    public GameObject lastPlatformHit;
+    public bool move = false;
+    private Animator anim;
+    public bool grounded = true;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GetComponent<Animator>().SetTrigger("Die");
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            move = !move;
+        }
+        //jumpY -= gravity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.velocity = new Vector3(0, jumpY, 0);
+            grounded = false;
+        }
+
+        if (move)
+        {
+            GetComponent<Animator>().SetBool("IsIdle", false);
+            float moveHorizontal = speed;
+            if (moveHorizontal != 0)
+            {
+                rb.velocity = new Vector3(moveHorizontal, rb.velocity.y, 0);
+            }
+        }
+        else
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            if(Mathf.Abs(moveHorizontal) > 0)
+                GetComponent<Animator>().SetBool("IsIdle", false);
+            if (Input.GetAxis("Horizontal") != 0)
+                rb.velocity = new Vector3(moveHorizontal * speed, rb.velocity.y, 0);
+
+
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Ground" || collider.gameObject.tag == "Obstacles")
+        {
+            grounded = true;
+            lastPlatformHit = collider.gameObject;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	[System.Serializable]
+	public class Player {
+		public int Health = 100;
+	}
+
+	public int fallBoundary = -20;
+
+	public Player playerP = new Player();
+
+	void Update(){
+		if (transform.position.y <= -20) {
+			TakeDamage(999999);
+		}
+	}
+	public void TakeDamage (int Damage){
+		playerP.Health -= Damage;
+		if (playerP.Health <= 0) {
+			GameMaster.KillPlayer();
+		}
+	}
+
+}
+
