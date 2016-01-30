7284d429d2a94208170902bca1d13306e2443223
Tin-Tin <kt-chin@hotmail.com>
2016-01-30 21:45:21 +0000

    Changed tilings, added day and night

---
+++ Assets/Scripts/DaynNight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DaynNight : MonoBehaviour {
+        private Camera cm;
+    
+	// Use this for initialization
+	void Start () {
+        cm = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Color colour1 = Color.blue * Color.clear - Color.black + new Color(.3f, .4f, .6f);
+        Color colour2 = Color.cyan * Color.white + new Color(.3f, .4f, .6f);
+        float duration = 30.0f;
+
+        cm.clearFlags = CameraClearFlags.SolidColor;
+        float time = Mathf.PingPong(Time.time, duration) / duration;
+        cm.backgroundColor = Color.Lerp(colour1, colour2, time);
+
+	}
+}
