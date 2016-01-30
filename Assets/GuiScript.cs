2bc6306b3a6349d268a547512767a803fdf8935c
Elio de Berardinis <eliodeb@gmail.com>
2016-01-30 11:30:53 +0000

    small stuff

---
+++ Assets/GuiScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiScript : MonoBehaviour {
+
+
+    public void OnStartGame()
+    {
+
+        Debug.Log("You pressed start");
+    }
+}
