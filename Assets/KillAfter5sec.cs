f48e115a64e3cb84365759886ac5d89eaea5bbeb
Jeyp <jp.evette1@gmail.com>
2016-01-31 10:06:21 +0000

    Adding lightning + bugfixes

---
+++ Assets/KillAfter5sec.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillAfter5sec : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(MyCoroutine());
+	}
+
+    IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
