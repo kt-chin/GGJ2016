d63cc10fa72f717daa3aa9b41b6a01fbee826b65
Jeyp <jp.evette1@gmail.com>
2016-01-31 01:35:14 +0000

    Castbar working

--- Assets/Elio/SpellHintScript.cs
+++ Assets/Elio/SpellHintScript.cs
@@ -4,20 +4,12 @@ using System.Collections;
 public class SpellHintScript : MonoBehaviour {
 
     public GameObject ArrowsContainer;
-    public GameObject DOWN;
-    public GameObject UP_HINT;
     public GameObject[] full_spell;
     public GameObject[] Hint_spells;
     public string key = "";
 
     string lightningStrike;
-
     
-    int x;
-    int i;
-    int i_max;
-    int max_x;
-    int min_x;
     bool is_destroyed = true;
 
     
@@ -30,14 +22,7 @@ public class SpellHintScript : MonoBehaviour {
 
         full_spell = new GameObject[5];
         Hint_spells = new GameObject[4];
-
-        i = 0;
-        i_max = 4;
-
-        x = -4;
-        max_x = 4;
-        min_x =-4;
-
+        
         
         
     }
@@ -48,54 +33,87 @@ public class SpellHintScript : MonoBehaviour {
 
         Create_Spell();
 
-       
 
-      
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            key += "U";
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            key += "D";
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            key += "L";
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            key += "R";
+        }
+
 
     }
 
 
     void Create_Spell()
     {
-        int spellID = gameObject.transform.parent.GetComponent<ComboSystem>().DetectSpellCast();
+        if(key.Length == 6)
+            key = "";
+
+        int spellID = gameObject.transform.parent.GetComponent<ComboSystem>().DetectSpellCast(key);
         string spellName = "";
-        if (spellID != -1)
+
+        clear_vector();
+        
+
+        for (int i = 0; i < key.Length; i++)
+        {
+            int keyMatch = 0;
+            if (key.Substring(i, 1) == "D")
+                keyMatch = 1;
+            if (key.Substring(i, 1) == "L")
+                keyMatch = 2;
+            if (key.Substring(i, 1) == "R")
+                keyMatch = 3;
+            int myX = -4 + (i*2);
+            full_spell[i] = (GameObject)Instantiate(ArrowsContainer.transform.GetChild(0).GetChild(keyMatch).gameObject, new Vector3(transform.parent.transform.position.x + myX, transform.parent.transform.position.y + 5, 0), Quaternion.identity);
+        }
+
+
+        if (spellID != -1 && key.Length > 0)
+        {
             spellName = gameObject.transform.parent.GetComponent<ComboSystem>().spellNames[spellID];
+            for (int i = key.Length; i < 5; i++)
+            {
+                int keyMatch = 0;
+                if (spellName.Substring(i, 1) == "D")
+                    keyMatch = 1;
+                if (spellName.Substring(i, 1) == "L")
+                    keyMatch = 2;
+                if (spellName.Substring(i, 1) == "R")
+                    keyMatch = 3;
+                int myX = -4 + (i * 2);
+                full_spell[i] = (GameObject)Instantiate(ArrowsContainer.transform.GetChild(1).GetChild(keyMatch).gameObject, new Vector3(transform.parent.transform.position.x + myX, transform.parent.transform.position.y + 5, 0), Quaternion.identity);
+            }
+        }
 
 
-        if      (Input.GetKeyDown(KeyCode.UpArrow)){ if (is_destroyed == false) { clear_vector(); } key += "U";  full_spell[i] = (GameObject)Instantiate(ArrowsContainer.transform.GetChild(0).GetChild(0).gameObject, new Vector3(transform.parent.transform.position.x + x, transform.parent.transform.position.y + 3, 0), Quaternion.identity); increment_position();}
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) { if (is_destroyed == false) { clear_vector(); } key += "D"; full_spell[i] = (GameObject)Instantiate(ArrowsContainer.transform.GetChild(0).GetChild(1).gameObject, new Vector3(transform.parent.transform.position.x + x, transform.parent.transform.position.y + 3 , 0), Quaternion.Euler(0,0,180)); increment_position();}
-        else if (Input.GetKeyDown(KeyCode.LeftArrow)) { if (is_destroyed == false) { clear_vector(); } key += "L"; full_spell[i] = (GameObject)Instantiate(ArrowsContainer.transform.GetChild(0).GetChild(2).gameObject, new Vector3(transform.parent.transform.position.x + x, transform.parent.transform.position.y + 3, 0), Quaternion.Euler(0,0,90)); increment_position();}
-        else if (Input.GetKeyDown(KeyCode.RightArrow)) { if (is_destroyed == false) { clear_vector(); } key += "R"; full_spell[i] = (GameObject)Instantiate(ArrowsContainer.transform.GetChild(0).GetChild(3).gameObject, new Vector3(transform.parent.transform.position.x + x, transform.parent.transform.position.y + 3, 0), Quaternion.Euler(0,0,-90)); increment_position(); }
+
         for (int u = 0; u < full_spell.Length; u++)
             if (full_spell[u] != null && full_spell[u].transform.parent != this.transform)
             { full_spell[u].transform.parent = this.transform;}
+
     }
+
 
    
 
-    
-
-    void increment_position() {
-
-        if (x < max_x && i < i_max)
-        {  x = x + 2;
-            i++;
-        }
-
-        else {
-
-            x = min_x;
-            i = 0;
-            is_destroyed = false; 
-        }
-
-    }
+   
 
 
     void clear_vector()
     {
-        key = "";
 
         for (int j = 0; j < 5; j++)
         {
