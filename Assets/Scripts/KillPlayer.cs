4242be19db1d365bb4d82ae01391f38a47e4ea0f
kt-chin <kt-chin@hotmail.com>
2016-01-31 07:01:14 +0000

    Commit

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -5,7 +5,7 @@ public class KillPlayer : StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        GameMaster.KillPlayer();
+       GameMaster.KillPlayer();
     }
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
